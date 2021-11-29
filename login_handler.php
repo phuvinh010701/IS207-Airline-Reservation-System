83dd6766da36bba2f8284ed50c2ad305b2a5e675
UIT.19522524 <58134921+phuvinh010701@users.noreply.github.com>
2021-11-29 11:53:54 +0700

    update login_handler.php no error

--- login_handler.php
+++ login_handler.php
@@ -9,68 +9,56 @@
 			session_start();
 			if(isset($_POST['Login']))
 			{
-				$data_missing=array();
-				$_SESSION['user_type'] = $user_type
-				if(empty($data_missing))
+				$user_type = $_POST['user_type'];
+				$user_name=$_POST['username'];
+				$pass_word=$_POST['password'];
+				$_SESSION['user_type'] = $user_type;
+				if($user_type=='Customer')
 				{
-					if($user_type=='Customer')
+					require_once('Database Connection file/mysqli_connect.php');
+					$query="SELECT count(*) FROM Customer where customer_id=? and pwd=?";
+					$stmt=mysqli_prepare($dbc,$query);
+					mysqli_stmt_bind_param($stmt,"ss",$user_name,$pass_word);
+					mysqli_stmt_execute($stmt);
+					mysqli_stmt_bind_result($stmt,$cnt);
+					mysqli_stmt_fetch($stmt);
+					mysqli_stmt_close($stmt);
+					mysqli_close($dbc);
+					
+					if($cnt==1)
 					{
-						require_once('Database Connection file/mysqli_connect.php');
-						$query="SELECT count(*) FROM Customer where customer_id=? and pwd=?";
-						$stmt=mysqli_prepare($dbc,$query);
-						mysqli_stmt_bind_param($stmt,"ss",$user_name,$pass_word);
-						mysqli_stmt_execute($stmt);
-						mysqli_stmt_bind_result($stmt,$cnt);
-						mysqli_stmt_fetch($stmt);
-						mysqli_stmt_close($stmt);
-						mysqli_close($dbc);
-						
-						if($cnt==1)
-						{
-							echo "Logged in <br>";
-							$_SESSION['login_user']=$user_name;
-							echo $_SESSION['login_user']." is logged in";
-							header("location: customer_homepage.php");
-						}
-						else
-						{
-							echo "Login Error";
-							session_destroy();
-							header('location:login_page.php?msg=failed');
-						}
+						$_SESSION['login_user']=$user_name;
+						header("location: customer_homepage.php");
 					}
-					else if($user_type=='Administrator')
+					else
 					{
-						require_once('Database Connection file/mysqli_connect.php');
-						$query="SELECT count(*) FROM Admin where admin_id=? and pwd=?";
-						$stmt=mysqli_prepare($dbc,$query);
-						mysqli_stmt_bind_param($stmt,"ss",$user_name,$pass_word);
-						mysqli_stmt_execute($stmt);
-						mysqli_stmt_bind_result($stmt,$cnt);
-						mysqli_stmt_fetch($stmt);
-						mysqli_stmt_close($stmt);
-						mysqli_close($dbc);
-						
-						if($cnt==1)
-						{
-							echo "Logged in <br>";
-							$_SESSION['login_user']=$user_name;
-							echo $_SESSION['login_user']." is logged in";
-							header('location:admin_homepage.php');
-						}
-						else
-						{
-							echo "Login Error";
-							session_destroy();
-							header('location:login_page.php?msg=failed');
-						}
+						session_destroy();
+						header('location:login_page.php?msg=failed');
 					}
 				}
-				
-			}
-			else
-			{
-				echo "Submit request not received";
+				else if($user_type=='Administrator')
+				{
+					require_once('Database Connection file/mysqli_connect.php');
+					$query="SELECT count(*) FROM Admin where admin_id=? and pwd=?";
+					$stmt=mysqli_prepare($dbc,$query);
+					mysqli_stmt_bind_param($stmt,"ss",$user_name,$pass_word);
+					mysqli_stmt_execute($stmt);
+					mysqli_stmt_bind_result($stmt,$cnt);
+					mysqli_stmt_fetch($stmt);
+					mysqli_stmt_close($stmt);
+					mysqli_close($dbc);
+					
+					if($cnt==1)
+					{
+						$_SESSION['login_user']=$user_name;
+						header('location:admin_homepage.php');
+					}
+					else
+					{
+						session_destroy();
+						header('location:login_page.php?msg=failed');
+					}
+				}
 			}
 		?>
 	</body>
